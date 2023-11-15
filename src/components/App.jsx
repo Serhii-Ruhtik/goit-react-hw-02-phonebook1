0d56e48aac34bde3adb833d8bcacfbc78a78d44c
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-11-15 21:11:50 +0200

    qwerty

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import css from './Phonebook.module.css';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return (
+      <div className={css.container}>
+        <h2 className={css.title}>Phonebook</h2>
+        <form className={css.form} action="">
+          <label className={css.label} htmlFor="">
+            Name
+          </label>
+          <input className={css.input} type="text" name="name" required />
+          <button className={css.button}>Add contact</button>
+        </form>
+        <h2 className={css.title}>Contacts</h2>
+        <ul className={css.list}>
+          <li className={css.item}>Homer Simpson</li>
+          <li className={css.item}>Marge Simpson</li>
+          <li className={css.item}>Bart Simpson</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default App;
